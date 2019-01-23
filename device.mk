bca66f828c976381d960f6fe136821208ad5744e
Bruno Martins <bgcngm@gmail.com>
2019-01-23 10:25:44 +0000

    cepheus: Implement SunlightEnhancement LiveDisplay HAL
    Change-Id: I17b0215ad78d86989c146291552c5e6e7b6a689d
    Signed-off-by: Arian <arian.kulmer@web.de>
    Signed-off-by: EcrosoftXiao <xy19990903@gmail.com>

--- device.mk
+++ device.mk
@@ -258,6 +258,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.cepheus
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service.cepheus
+
 # Media
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
