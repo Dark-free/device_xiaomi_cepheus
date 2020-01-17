9308288d7cfb50dc39d199899d264f10868c8ab4
Arthur Ishiguro <arthuri@google.com>
2020-01-17 14:48:25 -0800

    cepheus: Adds the Context Hub feature to cepheus
    Bug: 147677675
    Test: Compile only
    Change-Id: I08ce23d459ab6118f03a5e054de302331fa77571

--- device.mk
+++ device.mk
@@ -121,6 +121,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++.so
 
+# Context hub HAL
+PRODUCT_PACKAGES += \
+    android.hardware.contexthub@1.0-impl.generic \
+    android.hardware.contexthub@1.0-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.context_hub.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.context_hub.xml
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
