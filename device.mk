16b84674aba1b5e04925b1bacd3e964ddaad0ce7
Pig <pig.priv@gmail.com>
2020-09-19 08:44:33 +0800

    cepheus: Import vendor/qcom/opensource/commonsys/packages/apps/Bluetooth soong namespace
    Change-Id: I6bf3e1dda6fe5dc66f6fafdb32a1daecb9616c84

--- device.mk
+++ device.mk
@@ -378,7 +378,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/xiaomi \
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
 
 # Telephony
 PRODUCT_PACKAGES += \
