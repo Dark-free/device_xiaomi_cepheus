74931e3820085c433e40c48df7edd04bd5e37e6d
EcrosoftXiao <xy19990903@gmail.com>
2020-09-27 10:52:12 +0800

    cepheus: Add thermal config for thermal hal v2.0
    Signed-off-by: EcrosoftXiao <xy19990903@gmail.com>

--- device.mk
+++ device.mk
@@ -387,6 +387,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.cepheus
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
