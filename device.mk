c4678b77dae677037236c35f98d590f04a52d302
EcrosoftXiao <xy19990903@gmail.com>
2020-09-27 07:58:53 +0000

    cepheus: Bring back power-libperfmgr

--- device.mk
+++ device.mk
@@ -294,10 +294,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi_msmnile
+    android.hardware.power@1.3-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power/config/msmnile/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Product characteristics
 PRODUCT_CHARACTERISTICS := nosdcard
