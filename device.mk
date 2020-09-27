ee425a1850569e2363f4916d4947730119d11265
EcrosoftXiao <xy19990903@gmail.com>
2020-09-27 09:39:24 +0800

    cepheus: Build thermal hal v2.0
    * android.hardware.thermal@1.0::IThermal/default is deprecated in
     compatibility matrix at FCM Version 4; it should not be served.
    
    Signed-off-by: EcrosoftXiao <xy19990903@gmail.com>

--- device.mk
+++ device.mk
@@ -385,11 +385,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
-
-PRODUCT_PACKAGES += \
-    thermal.msmnile
+    android.hardware.thermal@2.0-service.cepheus
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
