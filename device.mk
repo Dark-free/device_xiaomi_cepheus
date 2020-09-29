8c85152ce6f9e946ab3e19583e6edd038ae98622
Volodymyr Zhdanov <wight554@gmail.com>
2020-09-29 21:47:10 +0300

    cepheus: add bt_stack_qti.conf to soong namespaces
    bt_stack_config: init attempt to load stack conf from /etc/bluetooth/bt_stack_qti.conf
    bt_osi_config: config_new unable to open file '/etc/bluetooth/bt_stack_qti.conf': No such file or directory
    bt_stack_config: init file >/etc/bluetooth/bt_stack_qti.conf< not found

--- device.mk
+++ device.mk
@@ -391,7 +391,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi \
-    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+    vendor/qcom/opensource/commonsys/system/bt/conf
 
 # Telephony
 PRODUCT_PACKAGES += \
