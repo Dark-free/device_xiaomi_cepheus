1afa8d0774ce9b487251ce2d99bbd72c76668352
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-10-09 06:13:37 +0000

    cepheus: Switch to AIDL Pixel PowerHAL
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- device.mk
+++ device.mk
@@ -329,7 +329,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi-libperfmgr \
+    android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0-service.xiaomi
 
 PRODUCT_COPY_FILES += \
@@ -394,6 +394,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
     hardware/xiaomi \
     vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
     vendor/qcom/opensource/commonsys/system/bt/conf
