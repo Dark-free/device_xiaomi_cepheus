f98cc9ab3df7cc9f61a5f7489b62875f3de0d37e
Akash Srivastava <akashniki@gmail.com>
2020-11-14 16:29:04 +0100

    cepheus: Update Media and Configstore blobs to LA.UM.9.1.r1-07300-SMxxx0.0
    This fixes media playback issue with new R stack

--- device.mk
+++ device.mk
@@ -292,6 +292,8 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    libcodec2_vndk.vendor \
+    libcodec2_hidl@1.0.vendor \
     libavservices_minijail \
     libavservices_minijail_32 \
     libavservices_minijail_vendor
