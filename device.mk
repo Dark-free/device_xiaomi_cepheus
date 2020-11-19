cfa51599e68e6807f58f95eeb795e2546c48e242
EcrosoftXiao <xy19990903@gmail.com>
2020-11-19 16:42:53 +0800

    Revert "cepheus: add bt_stack_qti.conf to soong namespaces"
    This reverts commit 8c85152ce6f9e946ab3e19583e6edd038ae98622.

--- device.mk
+++ device.mk
@@ -396,8 +396,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/xiaomi \
-    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
-    vendor/qcom/opensource/commonsys/system/bt/conf
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
 
 # Telephony
 PRODUCT_PACKAGES += \
