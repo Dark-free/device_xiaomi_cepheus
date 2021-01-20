d3023ca06a6121f9b1902e0575225038abab7f5d
Evasi0n Xiao <xy19990903@gmail.com>
2021-01-20 16:49:57 +0800

    cepheus: Build Soundtrigger@2.3

--- device.mk
+++ device.mk
@@ -44,7 +44,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
