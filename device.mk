94039a096783dddd5732f40d1ba469dfda9f94a3
EcrosoftXiao <xy19990903@gmail.com>
2021-01-25 14:10:52 +0800

    [Squash] Drop pixel thermal hal
    Revert "cepheus: thermal: cleanup thermal config"
    
    This reverts commit 2b896e6a53c064a3ef27ec5ccea6799b0c1f4866.
    
    Revert "cepheus: thermal: Higher quiet_therm HotThreshold"
    
    This reverts commit 82001042596f20f9a772f52a84db8b67b765c0f6.
    
    Revert "cepheus: Add thermal config for thermal hal v2.0"
    
    This reverts commit 74931e3820085c433e40c48df7edd04bd5e37e6d.
    
    Revert "cepheus: Build thermal hal v2.0"
    
    This reverts commit ee425a1850569e2363f4916d4947730119d11265.
    
    Revert "cepheus: thermal: reband to Xiaomi"
    
    This reverts commit a9e6257a8e8851fa405a2251eec1b5268ba922f5.
    
    Revert "cepheus: Import pixel thermal hal"
    
    This reverts commit dc3069ed91e9443e18cfac00099335fc389b9c6e.

--- device.mk
+++ device.mk
@@ -445,10 +445,11 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.cepheus
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+PRODUCT_PACKAGES += \
+    thermal.msmnile
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
