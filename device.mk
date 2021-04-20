3148b00b973301d5bbf642d1d676431c5ea071ac
Dark-free <emchaouki@gmail.com>
2021-04-20 19:16:12 -0400

    add kangos overlay && Kangos copyright

--- device.mk
+++ device.mk
@@ -352,7 +352,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay-kangos \
 
 # Power
 PRODUCT_PACKAGES += \
