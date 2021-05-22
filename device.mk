2eec9e105d23abf89265823e1cc58d85a5eda62b
Dark-free <emchaouki@gmail.com>
2021-05-22 15:51:56 +0100

    cepheus: Add overlay for masking the notch

--- device.mk
+++ device.mk
@@ -326,6 +326,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+		# Overlays
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
